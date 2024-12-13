fbd5df1bfa3932a4eae73a8595ea1de528d3717e
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2024-12-13 17:25:54 +0100

    subida reto 4 T2M4

--- poryecto_node/index.js
+++ poryecto_node/index.js
@@ -1,9 +1,36 @@
-//exporto
-const { writeAndRead } = require('./writeAndReadObject');
-const { readConsole } = require('./readConsole');
+// //exporto
+// const { writeAndRead } = require('./writeAndReadObject');
+// const { readConsole } = require('./readConsole');
 
-// Usar readConsole para obtener datos del usuario
-readConsole((user) => {
-    // Escribir y leer el objeto en un archivo
-    writeAndRead('./miFichero.json', user);
-});
+// // Usar readConsole para obtener datos del usuario
+// readConsole((user) => {
+//     // Escribir y leer el objeto en un archivo
+//     writeAndRead('./miFichero.json', user);
+// });
+
+
+//then/catch
+// const { writeAndRead } = require('./writeAndReadObject');
+// const { readConsole } = require('./readConsole');
+
+// function main() {
+//     readConsole((user) => {
+//         writeAndRead('./miFichero.json', user);
+//     });
+// }
+
+// main();
+
+
+//async/await
+
+// const { writeAndRead } = require('./writeAndReadObject');
+// const { readConsole } = require('./readConsole');
+
+// async function main() {
+//     await readConsole(async (user) => {
+//         await writeAndRead('./miFichero.json', user);
+//     });
+// }
+
+// main();
